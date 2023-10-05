7164eceb68fc19522f8bb32ce6db7ead6bcee63b
sᴧᴛᴧɴ ᴋσʟʟᴧ -✗ <144203735+SatanKolla666@users.noreply.github.com>
2023-10-05 21:05:10 +0530

    Update block.py

--- SatanMusic/plugins/block.py
+++ SatanMusic/plugins/block.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import add_gban_user, remove_gban_user
-from AnonX.utils.decorators.language import language
+from SatanMusic import app
+from SatanMusic.misc import SUDOERS
+from SatanMusic.utils.database import add_gban_user, remove_gban_user
+from SatanMusic.utils.decorators.language import language
 
 # Command
 BLOCK_COMMAND = get_command("BLOCK_COMMAND")
